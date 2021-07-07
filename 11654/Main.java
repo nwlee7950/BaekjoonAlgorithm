d1a263dc1c87047a60620ecde1deb7d6a6393606
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-07-07 10:26:21 +0900

    아스키 코드

---
+++ 11654/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char n;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextLine().charAt(0);
+		
+		System.out.println((int)n);
+	}
+}
