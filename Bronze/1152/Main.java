4e0fecf97bb6da1e3ef35cadde8b92c06444189b
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-07-25 21:59:27 +0900

    단어의 개수

---
+++ Bronze/1152/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		 String str = sc.nextLine().trim();
+		 
+		 if(str.isEmpty())
+			 System.out.println(0);
+		 else
+			 System.out.println(str.split(" ").length);
+	}
+}
