45e6a5cb0cc909f67fbc6341d36560d7ab793c9e
이승현 <nwlee7950@naver.com>
2021-08-03 22:23:52 +0900

    하노이 탑 이동 순서

---
+++ Silver/11729/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+
+	static StringBuilder sb = new StringBuilder();
+	static int count = 0;
+
+	public static void hanoi(int n, int from, int to, int temp) {
+		if (n == 0)
+			return;
+		else {
+			count++;
+			hanoi(n - 1, from, temp, to);
+			sb.append(from + " " + to + "\n");
+			hanoi(n - 1, temp, to, from);		
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		hanoi(n, 1, 3, 2);
+
+		System.out.println(count);
+		System.out.println(sb);
+	}
+}
