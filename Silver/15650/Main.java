128b5e56ea3169eecaf61634f764087ea9220846
이승현 <nwlee7950@naver.com>
2021-08-12 23:40:39 +0900

    N과 M(2)

---
+++ Silver/15650/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	private static void solve(int idx, int start) {
+		if(idx == M) {
+			for(int i=0; i<M; i++) 
+					sb.append(arr[i]).append(" ");
+			sb.append("\n");	
+			return;
+		}
+		for(int i=start; i<=N; i++) {
+			arr[idx] = i;
+			solve(idx + 1, i+1);
+		}
+	}
+	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] str = br.readLine().split(" ");
+        N = Integer.parseInt(str[0]);
+        M = Integer.parseInt(str[1]);
+        arr = new int[M];
+
+        solve(0, 1);
+        System.out.println(sb);
+    }
+}
+
