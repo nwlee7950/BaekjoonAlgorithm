7f040e3117a1bd33eca16e464da9b7ca58002aa1
이승현 <nwlee7950@naver.com>
2021-09-15 15:22:22 +0900

    이동하기

---
+++ Silver/11048/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException   {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int arr[][] = new int[n+1][m+1];
+		int dp[][] = new int[n+1][m+1];
+		
+		for(int i=1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=1; j<=m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				dp[i][j] = Math.max( dp[i-1][j] , Math.max( dp[i-1][j-1], dp[i][j-1] ) ) + arr[i][j];
+			}
+		}
+		
+		System.out.println(dp[n][m]);
+	}
+}
